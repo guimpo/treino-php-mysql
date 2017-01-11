ca5290e04b673ebeff59495a6fbdd61ac1f49b93
guimpo <nakaima.prk@gmail.com>
2017-01-11 14:30:21 -0200

    Isolando cabeçalho e rodapé

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,13 +1,3 @@
-<html>
-	<head>
-		<title>Minha Loja</title>
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/loja.css">
-	</head>
-
-	<body>
-		<div class="container">
-			<div class="principal">
 				<?php 
 					$nome =  $_GET["nome"];
 					$preco = $_GET["preco"];
@@ -15,8 +5,3 @@
 				<p class="alert-success">
 					Produto <?=  $nome; ?>, <?= $preco; ?> adicionado com sucesso!
 				</p>
-			</div>
-		</div>
-
-	</body>
-</html>
---
+++ cabecalho.php
@@ -0,0 +1,10 @@
+<html>
+	<body>
+		<div class="container">
+			<div class="principal">
+				<head>
+					<title>Minha Loja</title>
+						<meta charset="utf-8">
+						<link rel="stylesheet" href="css/bootstrap">
+						<link rel="stylesheet" href="css/loja.css">	
+					</head>
