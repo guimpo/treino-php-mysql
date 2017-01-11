3875518f620fe360b4381c04e9cb04b3c9c98a54
guimpo <nakaima.prk@gmail.com>
2017-01-11 12:06:14 -0200

    Aplicando css em adiciona-produto

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,9 +1,21 @@
 <html>
-<?php 
-	$nome =  $_GET["nome"];
-	$preco = $_GET["preco"];
-?>
+	<head>
+		<title>Minha Loja</title>
+		<link rel="stylesheet" href="css/bootstrap">
+		<link rel="stylesheet" href="css/loja.css">
+	</head>
 
-	Produto <?=  $nome; ?>, <?= $preco; ?> adicionado com sucesso!
+	<body>
+		<div class="container">
+			<div class="principal">
+				<?php 
+					$nome =  $_GET["nome"];
+					$preco = $_GET["preco"];
+				?>
+			</div>
+		</div>
+		<p class="alert-sucess">
+			Produto <?=  $nome; ?>, <?= $preco; ?> adicionado com sucesso!
+		</p>
+	</body>
 </html>
-
