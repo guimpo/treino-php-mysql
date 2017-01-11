4c43a6e94ae49fb6ea474518a70a2d0d0a1a08c0
guimpo <nakaima.prk@gmail.com>
2017-01-11 17:43:05 -0200

    Refatorando

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -1,24 +1,28 @@
 <?php include("cabecalho.php"); ?>
-	<?php 
-		$nome =  $_GET["nome"];
-		$preco = $_GET["preco"];
-		$conexao = mysqli_connect('localhost', 'root', '', 'loja');
-		$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
-		$retorno_query = mysqli_query($conexao, $query);
-		mysqli_close($conexao);
-	?>
-	<?php
-		if($retorno_query) {
-	?>
-	<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
-	<?php	
-		} else {
-	?>
-	<p class="alert-danger">
-		Produto <?=  $nome; ?>, <?= $preco; ?> não foi adicionado!
-	</p>
-	<?php
-		}
-	?>
+<?php 
+function insereProduto($conexao, $nome, $preco) {
+	$query = "insert into produtos (nome, preco) values ('{$nome}', {$preco})";
+	return  mysqli_query($conexao, $query);
+}
+?>
+<?php 
+	$nome =  $_GET["nome"];
+	$preco = $_GET["preco"];
+	$conexao = mysqli_connect('localhost', 'root', '', 'loja');
+	$mysqli_close($conexao);
+?>
+<?php
+	if(insereProduto($conexa, $nome, $preco) {
+?>
+<p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+<?php	
+	} else {
+?>
+<p class="alert-danger">
+	Produto <?=  $nome; ?>, <?= $preco; ?> não foi adicionado!
+</p>
+<?php
+	}
+?>
 <?php include("rodape.php"); ?>
 	
