105e406b7272a590ecad582fecbed9353ca476aa
guimpo <nakaima.prk@gmail.com>
2017-01-11 13:02:35 -0200

    Aplicando css em produto-formulario

--- produto-formulario.php
+++ produto-formulario.php
@@ -1,7 +1,20 @@
 <html>
-	<form action="adiciona-produto.php">
-		Nome: <input type="text" name="nome" /><br/>
-		Preço: <input type="number" name="preco" /><br/>
-		<input type="submit" value="Cadastrar" />
-	</form>
+	<head>
+		<title>Minha Loja</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="css/bootstrap">
+		<link rel="stylesheet" href="css/loja.css">	
+	</head>
+	<body>
+		<div class="container">
+			<div class="principal">
+				<h1>Formulário de Cadastro</h1>
+				<form action="adiciona-produto.php">
+					Nome: <input type="text" name="nome" /><br/>
+					Preço: <input type="number" name="preco" /><br/>
+					<input type="submit" value="Cadastrar" />
+				</form>
+			</div>
+		</div>
+	</body>
 </html>
