5480f548ff37b88b3122873c659f5c46290b3db1
guimpo <nakaima.prk@gmail.com>
2017-01-11 10:58:14 -0200

    Adicionando estilo na página principal

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <html>
-	<h1>Bem vindo!</h1>
-</html>
-
+<head>
+	<title>Minha Loja</title>
+	<meta charset="utf-8">
+	<link href="css/bootstrap.css" rel="stylesheet" />
+	<link href="css/loja.css" rel="stylesheet" />
+<body>
+	<div class="container">
+		<div class="principal">
+			<h1>Bem Vindo!</h1>
+		</div>
+	</div>
+</body>
