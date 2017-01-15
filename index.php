f0a1b28bbd9cf88a57d0025a77ff14e5b2422352
guimpo <nakaima.prk@gmail.com>
2017-01-15 09:13:02 -0200

    Criado redirecionamento de usuário

--- index.php
+++ index.php
@@ -1,4 +1,19 @@
 <?php include("cabecalho.php"); ?>
+<?php
+if(isset($_GET["login"]) && $_GET["login"]==1) {
+?>
+<p class="alert-success">Você está logado!</p>
+<?php
+}
+?>
+<?php
+if(isset($_GET["login"]) && $_GET["login"]==0) {
+?>
+<p class="alert-danger">Usuário ou senha incorreta</p>
+<?php
+}
+?>
+
 	<h1>Bem vindo!</h1>
 <table class="table">
 	<form action="login.php" method="post">
