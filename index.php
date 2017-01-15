48aae8b8cb777cadb242b45288634a10e21227a5
guimpo <nakaima.prk@gmail.com>
2017-01-15 08:02:16 -0200

    Criado formulário de login

--- index.php
+++ index.php
@@ -1,3 +1,18 @@
 <?php include("cabecalho.php"); ?>
 	<h1>Bem vindo!</h1>
+<table class="table">
+	<form action="login.php" method="post">
+		<tr>
+			<td>Email</td>
+				<td><input type="email" name="email" class="form-control"></td>
+		</tr>
+		<tr>
+			<td>Senha</td>
+			<td><input type="password" name="senha" class="form-control"></td>
+		</tr>
+		<tr>
+			<td><button class="btn btn-primary" type="submit" class="form-control">Logar</button></td>
+		</tr>
+	</form>
+</table>
 <?php include("rodape.php"); ?>
