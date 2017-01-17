b779c107f4c49e3a6f80e88c3547323881c56e71
guimpo <nakaima.prk@gmail.com>
2017-01-17 16:19:51 -0200

    Tratando sql injection

--- banco-produto.php
+++ banco-produto.php
@@ -9,22 +9,32 @@
 }
 
 function insereProduto($conexao, $nome, $preco, $descricao, $categoria, $usado) {
+	$nome = mysqli_real_escape_string($conexao, $nome);
+	$preco = mysqli_real_escape_string($conexao, $preco);
+	$descricao = mysqli_real_escape_string($conexao, $descricao);	
+	$categoria = mysqli_real_escape_string($conexao, $categoria);
 	$query = "insert into produtos (nome, preco, descricao, categoria_id, usado) values ('{$nome}', {$preco}, '{$descricao}', {$categoria}, {$usado})";
 	return  mysqli_query($conexao, $query);
 }
 
 function buscaProduto($conexao, $id) {
+	$id = mysqli_real_escape_string($conexao, $id);
 	$query = "SELECT * FROM produtos WHERE id = {$id}";
 	$resultado =  mysqli_query($conexao, $query);
 	return mysqli_fetch_assoc($resultado);
 }
 
 function alteraProduto($conexao, $id, $nome, $preco, $descricao, $categoria, $usado) {
+	$nome = mysqli_real_escape_string($conexao, $nome);
+	$preco = mysqli_real_escape_string($conexao, $preco);
+	$descricao = mysqli_real_escape_string($conexao, $descricao);	
+	$categoria = mysqli_real_escape_string($conexao, $categoria);
 	$query = "UPDATE produtos SET nome = '{$nome}', preco = {$preco}, descricao = '{$descricao}', categoria_id = {$categoria}, usado = {$usado} WHERE id = {$id}";
 	return mysqli_query($conexao, $query);
 }
 
 function removeProduto($conexao, $id) {
+	$id = mysqli_real_escape_string($conexao, $id);
 	$query = "DELETE FROM produtos WHERE id = {$id}";
 	return mysqli_query($conexao, $query);
 }
