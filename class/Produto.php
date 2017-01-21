b7eae42280ae5341963611b99c60bbd48e2d9462
guimpo <nakaima.prk@gmail.com>
2017-01-21 00:17:59 -0200

    criadas classes produto e categoria

---
+++ class/Produto.php
@@ -0,0 +1,28 @@
+<<?php
+
+class Produto {
+
+  private $id;
+  private $nome;
+  private $preco;
+  private $descricao;
+  private $usado;
+  private $categoria;
+
+  function __construct($nome, $preco, $descricao, $usado, Categoria $categoria) {
+    $this->nome = $nome;
+    $this->preco = $preco;
+    $this->descricao = $descricao;
+    $this->usado = $usado;
+    $this->categoria = $categoria;
+  }
+
+  function getId() {
+    return $this->id;
+  }
+  function setId($id) {
+    $this->id = $id
+  }
+}
+
+?>
