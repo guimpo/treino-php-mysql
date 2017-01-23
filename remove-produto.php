f1f48b58e35537c3026d58806df4739add80260d
guimpo <nakaima.prk@gmail.com>
2017-01-23 14:36:27 -0200

    Refatorado removeProduto para padrão dao

--- remove-produto.php
+++ remove-produto.php
@@ -1,14 +1,16 @@
 <?php
 require_once("cabecalho.php");
-require_once("banco-produto.php");
 require_once("logica-usuario.php");
 
+$produtoDao = new ProdutoDao($conexao);
+
 $categoria = new Categoria($categoria_id, $categoria_nome);
+
 $produto = new Produto($produto_nome, $produto_preco, $produto_descricao, $produto_usado, $categoria);
 
 $produto->setId($_POST["id"]);
 
-if(removeProduto($conexao, $produto)) :
+if($produtoDao->removeProduto($produto)) :
   header("Location: produto-lista.php");
   $_SESSION["success"] = "Produto removido!";
 else :
