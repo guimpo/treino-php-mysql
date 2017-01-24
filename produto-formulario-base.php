a54ea952534659c3c663b36483a22182d50c2a45
guimpo <nakaima.prk@gmail.com>
2017-01-24 14:11:00 -0200

    Inserido input de isbn e tipo

--- produto-formulario-base.php
+++ produto-formulario-base.php
@@ -1,6 +1,31 @@
 				<td><input class="form-control" type="text" name="nome" value="<?=  htmlentities($produto->getNome(), ENT_QUOTES); ?>"></td>
 			</tr>
 			<tr>
+				<td>Tipo</td>
+				<td>
+					<select class="form-control" name="tipo">
+						<?php
+							$tipos = array("Livro", "Produto");
+							foreach($tipos as $tipo) :
+								$tipo_do_produto = get_class($produto) == $tipo;
+								$seleciona = $tipo_do_produto ? "selected='selected'" : "";
+						?>
+								<option value="<?= $tipo ?>" <?= $seleciona ?>>
+									<?= $tipo ?>
+								</option>
+						<?php
+							endforeach
+						?>
+					</select>
+				</td>
+			</tr>
+			<tr>
+				<td>ISBN</td>
+				<td>
+					<input type="text" class="form-control" value="<?= $produto->isLivro() ? $produto->getIsbn() : ""; ?>">
+				</td>
+			</tr>
+			<tr>
 				<td>Preço</td>
 				<td><input class="form-control" type="number" name="preco" value=<?= $produto->getPreco() ?>></td>
 			</tr>
