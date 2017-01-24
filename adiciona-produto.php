c40b807f993ece3c72400ad729c1ff125331dee9
guimpo <nakaima.prk@gmail.com>
2017-01-24 14:19:37 -0200

    Adequado adiciona-produto para instanciar Livro

--- adiciona-produto.php
+++ adiciona-produto.php
@@ -9,7 +9,15 @@ $preco = $_POST["preco"];
 $descricao = $_POST["descricao"];
 $categoria = new Categoria($_POST["categoria_id"], $_POST["categoria_nome"]);
 $usado = $_POST["usado"];
-$produto = new Produto($nome, $preco, $descricao, $usado, $categoria);
+$tipo = $_POST["tipo"];
+$isbn = $_POST["isbn"];
+
+if($tipo == "Livro") :
+	$produto = new Livro($nome, $preco, $descricao, $usado, $categoria);
+	$produto->setIsbn($isbn);
+else :
+	$produto = new Produto($nome, $preco, $descricao, $usado, $categoria);
+endif;
 
 if($produtoDao->insereProduto($produto)) :
 	$_SESSION["success"] = "Produto {$nome}, {$preco}, adicionado com sucesso!";
